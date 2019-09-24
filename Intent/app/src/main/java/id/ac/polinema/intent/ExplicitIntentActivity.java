4c0fd810d855bcffff5e50f2a17c14b35a3a595e
chintyadewi <chintyadewi437@gmail.com>
2019-09-24 20:52:04 +0700

    melengkapi todo explicit, implisit, bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,18 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText input_nama;
+    private TextView output_nama;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        input_nama=findViewById(R.id.input_name);
+        output_nama=findViewById(R.id.text_output);
     }
 
+    public void submitName(View view) {
+        output_nama.setText(input_nama.getText().toString());
+    }
 }
